6b93e6e24909d13bf74292b75045665e38ca1b17
Safak <safakustatr@gmail.com>
2025-07-04 16:35:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,16 @@ import pandas
 
 data = pandas.read_csv("nato_phonetic_alphabet.csv")
 alphabet_dict = {row.letter:row.code for (index, row) in data.iterrows()}
-word = input("Enter a word: ").upper()
-output = [alphabet_dict[letter] for letter in word]
-print(output)
+
+program_on = True
+
+while program_on:
+    try:
+        word = input("Enter a word: ").upper()
+        output = [alphabet_dict[letter] for letter in word]
+        print(output)
+        choice = input("Do you wish to continue? (y/n): ").lower()
+        if choice == "n":
+            program_on = False
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
